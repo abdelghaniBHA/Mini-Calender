f4acdd12cf303819aa2d48b6dac1aeea8d89d19e
abdelghani <benhadjali@gmail.com>
2024-10-15 23:05:54 +0200

    solve the problem of days and month and style the date

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ const year = document.getElementById("year");
 
 const today = new Date();
 
+
+const weekDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+const Months = ["Junuary","February","March","April","May","June","July","August","September","October","November","December"];
+
 date.innerHTML = today.getDate();
-day.innerHTML = today.getDay();
-month.innerHTML = today.getMonth()+1;
+if(date<10){
+    date.innerHTML = "0" + today.getDate()
+}
+day.innerHTML = weekDays[today.getDay()]
+month.innerHTML = Months[today.getMonth()+1]
 year.innerHTML = today.getFullYear();
