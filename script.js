99fe6caf8ea25e70fc9ac0a4b3bc283cc5949802
abdelghani <benhadjali@gmail.com>
2024-10-15 19:07:34 +0200

    make the date dynamic

---
+++ script.js
@@ -0,0 +1,11 @@
+const date = document.getElementById("date");
+const day = document.getElementById("day");
+const month = document.getElementById("month");
+const year = document.getElementById("year");
+
+const today = new Date();
+
+date.innerHTML = today.getDate();
+day.innerHTML = today.getDay();
+month.innerHTML = today.getMonth()+1;
+year.innerHTML = today.getFullYear();
